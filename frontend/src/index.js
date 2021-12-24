ca44d0bff9a56dd14a249db5d729222e0ae9766a
qnen <gbrecci@gmail.com>
2021-12-24 02:01:55 -0300

    Removing comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
-/** VIDEO 3 TIME = 00:49:15 */
